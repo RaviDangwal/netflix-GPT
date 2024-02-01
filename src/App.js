7766f36e4eec437464eec983f377cb6864b18f37
RaviDangwal <ravidangwal8@gmail.com>
2024-02-01 11:21:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello. I will build netflix</h1>
+      <h2>Netflix-GPT loading....soon</h2>
+    </div>
+  );
+}
+
+export default App;
